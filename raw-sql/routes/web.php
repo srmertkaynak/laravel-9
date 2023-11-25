b86864a419ea6efc783c29af4727bfa569214ca7
Mert Kaynak <kaynak.mert35@gmail.com>
2023-11-26 02:55:54 +0300

    Route Sayfası ile Yönlendirmeler

--- raw-sql/routes/web.php
+++ raw-sql/routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PostController::class, "veriListesi"]);
+
+Route::get('/ekle', [PostController::class, "veriEkle"]);
+
+Route::get('/guncelle', [PostController::class, "veriGuncelle"]);
+
+Route::get('/sil', [PostController::class, "veriSil"]);
+
+Route::get('/hepsini-sil', [PostController::class, "hepsiniTemizle"]);
+
+Route::get('/tabloyu-sil', [PostController::class, "tabloyuSil"]);
